e6043ba2afd9fc72e7cdda5e736cde74984e41f8
William Andreoli <willandreoli33@gmail.com>
2024-02-06 10:40:02 -0300

    ProcessoJDBC deleteById and findAll implementation

--- src/application/Test3.java
+++ src/application/Test3.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.ProcessoDao;
 import model.entities.Processo;
@@ -9,12 +11,25 @@ public class Test3 {
 	public static void main(String[] args) {
 
 		System.out.println("=== TEST processo ====");
-		System.out.println("=== TEST 1: findById");
+		System.out.println("=== TEST 1: findById ====");
 		
 		ProcessoDao pDao = DaoFactory.createProcessoDao();
 		
 		Processo processo = pDao.findById(1);
 		System.out.println(processo);
+		
+		System.out.println("=== TEST 2: findAll ====");
+		
+		List<Processo> listP = pDao.findAll();
+	
+		for (Processo p  : listP) {
+			System.out.println(p);
+		}
+		
+		System.out.println("=== TEST 3: deleteById ====");
+		
+		pDao.deletebyId(2);
+		System.out.println("Deleted!");
 		
 	}
 
--- src/model/dao/impl/ProcessoJDBC.java
+++ src/model/dao/impl/ProcessoJDBC.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -13,6 +15,7 @@ import model.dao.ProcessoDao;
 import model.entities.Advogado;
 import model.entities.Cliente;
 import model.entities.Parte_Processo;
+import model.entities.Pessoa_Fisica;
 import model.entities.Processo;
 import model.entities.Tribunal;
 import model.entities.Usuario;
@@ -59,31 +62,30 @@ public class ProcessoJDBC implements ProcessoDao {
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement("SELECT processo.*, cliente.nome as Nome_Do_Cliente, advogado.nome as Nome_Do_Advogado, "
-				    + "parte_processo.nome as Nome_Da_Parte, tribunal.nome as Tribunal, usuario.login as Usuário "
-				    + "FROM processo "
-				    + "INNER JOIN cliente ON processo.cliente_Id = cliente.id "
-				    + "INNER JOIN advogado ON processo.advogado_Id = advogado.id "
-				    + "INNER JOIN parte_processo ON processo.partes = parte_processo.id "
-				    + "INNER JOIN tribunal ON processo.tribunal = tribunal.id "
-				    + "INNER JOIN usuario ON processo.usuario = usuario.id "
-				    + "WHERE processo.id = ?");
+			st = conn.prepareStatement(
+					"SELECT processo.*, cliente.nome as Nome_Do_Cliente, advogado.nome as Nome_Do_Advogado, "
+							+ "parte_processo.nome as Nome_Da_Parte, tribunal.nome as Tribunal, usuario.login as Usuário "
+							+ "FROM processo " + "INNER JOIN cliente ON processo.cliente_Id = cliente.id "
+							+ "INNER JOIN advogado ON processo.advogado_Id = advogado.id "
+							+ "INNER JOIN parte_processo ON processo.partes = parte_processo.id "
+							+ "INNER JOIN tribunal ON processo.tribunal = tribunal.id "
+							+ "INNER JOIN usuario ON processo.usuario = usuario.id " + "WHERE processo.id = ?");
 
 			st.setInt(1, id);
 			rs = st.executeQuery();
 			if (rs.next()) {
 				Cliente cliente = instantiateCliente(rs);
-				
+
 				Advogado advogado = instantiateAdvogado(rs);
-				
+
 				Parte_Processo parte = instantiatePartes(rs);
-				
+
 				Tribunal tribunal = instantiateTribunal(rs);
-				
+
 				Usuario usuario = instantiateUsuario(rs);
-				
+
 				Processo obj = instantiateProcesso(rs, cliente, advogado, parte, tribunal, usuario);
-				
+
 				return obj;
 			}
 			return null;
@@ -95,7 +97,8 @@ public class ProcessoJDBC implements ProcessoDao {
 		}
 	}
 
-	private Processo instantiateProcesso(ResultSet rs, Cliente cliente, Advogado advogado, Parte_Processo parte, Tribunal tribunal, Usuario usuario) throws SQLException {
+	private Processo instantiateProcesso(ResultSet rs, Cliente cliente, Advogado advogado, Parte_Processo parte,
+			Tribunal tribunal, Usuario usuario) throws SQLException {
 		Processo obj = new Processo();
 		obj.setId(rs.getInt("id"));
 		obj.setNumero_Processo(rs.getString("numero_Processo"));
@@ -113,35 +116,35 @@ public class ProcessoJDBC implements ProcessoDao {
 		obj.setUsuario(usuario);
 		return obj;
 	}
-	
+
 	private Cliente instantiateCliente(ResultSet rs) throws SQLException {
 		Cliente cliente = new Cliente();
 		cliente.setId(rs.getInt("id"));
 		cliente.setNome(rs.getString("Nome_Do_Cliente"));
 		return cliente;
 	}
-	
+
 	private Advogado instantiateAdvogado(ResultSet rs) throws SQLException {
 		Advogado advogado = new Advogado();
 		advogado.setId(rs.getInt("id"));
 		advogado.setNome(rs.getString("Nome_Do_Advogado"));
 		return advogado;
 	}
-	
+
 	private Parte_Processo instantiatePartes(ResultSet rs) throws SQLException {
 		Parte_Processo parte = new Parte_Processo();
 		parte.setId(rs.getInt("id"));
 		parte.setNome(rs.getString("Nome_Da_Parte"));
 		return parte;
 	}
-	
+
 	private Tribunal instantiateTribunal(ResultSet rs) throws SQLException {
 		Tribunal tribunal = new Tribunal();
 		tribunal.setId(rs.getInt("id"));
 		tribunal.setNome(rs.getString("Tribunal"));
 		return tribunal;
 	}
-	
+
 	private Usuario instantiateUsuario(ResultSet rs) throws SQLException {
 		Usuario obj = new Usuario();
 		obj.setId(rs.getInt("id"));
@@ -151,8 +154,71 @@ public class ProcessoJDBC implements ProcessoDao {
 
 	@Override
 	public List<Processo> findAll() {
-		return null;
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT processo.*, cliente.nome as Nome_Do_Cliente, advogado.nome as Nome_Do_Advogado, "
+					+ "parte_processo.nome as Nome_Da_Parte, tribunal.nome as Tribunal, usuario.login as Usuário "
+					+ "FROM processo " + "INNER JOIN cliente ON processo.cliente_Id = cliente.id "
+					+ "INNER JOIN advogado ON processo.advogado_Id = advogado.id "
+					+ "INNER JOIN parte_processo ON processo.partes = parte_processo.id "
+					+ "INNER JOIN tribunal ON processo.tribunal = tribunal.id "
+					+ "INNER JOIN usuario ON processo.usuario = usuario.id " 
+					+ "ORDER BY tipo");
 
+			rs = st.executeQuery();
+
+			List<Processo> list = new ArrayList<>();
+			Map<Integer, Cliente> mapCli = new HashMap<>();
+			Map<Integer, Advogado> mapAdv = new HashMap<>();
+			Map<Integer, Parte_Processo> mapPar = new HashMap<>();
+			Map<Integer, Tribunal> mapTri = new HashMap<>();
+			Map<Integer, Usuario> mapUser = new HashMap<>();
+
+			while (rs.next()) {
+
+				Cliente cliente = mapCli.get(rs.getInt("cliente_Id"));
+				Advogado advogado = mapAdv.get(rs.getInt("advogado_Id"));
+				Parte_Processo partes = mapPar.get(rs.getInt("partes"));
+				Tribunal tribunal = mapTri.get(rs.getInt("tribunal"));
+				Usuario usuario = mapUser.get(rs.getInt("usuario"));
+
+				if (cliente == null) {
+					cliente = instantiateCliente(rs);
+					mapCli.put(rs.getInt("cliente_Id"), cliente);
+				}
+				
+				if (advogado == null) {
+					advogado = instantiateAdvogado(rs);
+					mapAdv.put(rs.getInt("advogado_Id"), advogado);
+				}
+				
+				if (partes == null) {
+					partes = instantiatePartes(rs);
+					mapPar.put(rs.getInt("partes"), partes);
+				}
+				
+				if (tribunal == null) {
+					tribunal = instantiateTribunal(rs);
+					mapTri.put(rs.getInt("tribunal"), tribunal);
+				}
+				
+				if (usuario == null) {
+					usuario = instantiateUsuario(rs);
+					mapUser.put(rs.getInt("usuario"), usuario);
+				}
+
+				Processo obj = instantiateProcesso(rs, cliente, advogado, partes, tribunal, usuario);
+				list.add(obj);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 
 }
